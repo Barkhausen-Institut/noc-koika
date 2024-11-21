ae8c2515b75570f4abec8e3ebc73bac6228d3ab0
Sebastian Ertel <sebastian.ertel@gmail.com>
2024-11-21 12:07:32 +0100

    some progress on proving that our register types are finite

--- noc/helpers.v
+++ noc/helpers.v
@@ -5,66 +5,66 @@ Set Equations Transparent.
 
 Module Helpers.
 
-Equations absurd_fin {T} (x:Fin.t 0) : T := .
+  Equations absurd_fin {T} (x:Fin.t 0) : T := .
 
-Derive NoConfusion for nat.
-Derive Signature NoConfusion for Fin.t.
-Derive Signature for le.
+  Derive NoConfusion for nat.
+  Derive Signature NoConfusion for Fin.t.
+  Derive Signature for le.
 
-(* Equations cannot recurse on [Prop]. *)
-Fail Derive NoConfusion for le.
+  (* Equations cannot recurse on [Prop]. *)
+  Fail Derive NoConfusion for le.
 
-(* Set Equations Debug. *)
+  (* Set Equations Debug. *)
 
-Equations widen_fin {n n'} (H: n<=n') (x : Fin.t n) : Fin.t n' :=
-  widen_fin (n:=0)   (n':=0)   _H          x := x;
-  widen_fin (n:=S a) (n':=S a) (le_n a)    x := x;
-  widen_fin (n:=0)   (n':=S a) _H  x := absurd_fin x; (* absurd *)
-  widen_fin (n:=S a) (n':=0)   H           x := _; (* absurd *)
-  widen_fin (n:=S a) (n':=S b) (le_S b H1) x with widen_fin H1 x => {
-      widen_fin (n:=S a) (n':=S b) (le_S b H2) _x y := FS y
-   }.
-Next Obligation. lia.
-Fail Qed.
-(* Use [Set Equations Debug] to see more.
-   The problem is that Equations cannot recurse into [Prop] because
-   then the function become undefinable/extractable.
- *)
-Abort.
+  Equations widen_fin {n n'} (H: n<=n') (x : Fin.t n) : Fin.t n' :=
+    widen_fin (n:=0)   (n':=0)   _H          x := x;
+    widen_fin (n:=S a) (n':=S a) (le_n a)    x := x;
+    widen_fin (n:=0)   (n':=S a) _H  x := absurd_fin x; (* absurd *)
+    widen_fin (n:=S a) (n':=0)   H           x := _; (* absurd *)
+    widen_fin (n:=S a) (n':=S b) (le_S b H1) x with widen_fin H1 x => {
+        widen_fin (n:=S a) (n':=S b) (le_S b H2) _x y := FS y
+      }.
+  Next Obligation. lia.
+                   Fail Qed.
+  (* Use [Set Equations Debug] to see more.
+     The problem is that Equations cannot recurse into [Prop] because
+     then the function become undefinable/extractable.
+   *)
+  Abort.
 
-(* We recover from this situation by redefining [le] in [Type]. *)
-Reserved Notation "a <<= b" (at level 99).
-Inductive le_t (n : nat) : nat -> Type :=
+  (* We recover from this situation by redefining [le] in [Type]. *)
+  Reserved Notation "a <<= b" (at level 99).
+  Inductive le_t (n : nat) : nat -> Type :=
   | le_n : n <<= n
   | le_S : forall m : nat, n <<= m -> n <<= S m
-where "a <<= b" := (le_t a b).
+  where "a <<= b" := (le_t a b).
 
-Derive Signature NoConfusion for le_t.
+  Derive Signature NoConfusion for le_t.
 
-Equations absurd_le_t {n:nat} {T} (H: (S n) <<= 0) : T := .
+  Equations absurd_le_t {n:nat} {T} (H: (S n) <<= 0) : T := .
 
-Equations widen_fin {n n'} (H: n<<=n') (x : Fin.t n) : Fin.t n' :=
-  widen_fin (n:=0)   (n':=0)   _H          x := x;
-  widen_fin (n:=S a) (n':=S a) (le_n a)    x := x;
-  widen_fin (n:=0)   (n':=S a) _H  x := absurd_fin x; (* absurd *)
-  widen_fin (n:=S a) (n':=0)   H           x := _; (* absurd *)
-  widen_fin (n:=S a) (n':=S b) (le_S b H1) x with widen_fin H1 x => {
-      widen_fin (n:=S a) (n':=S b) (le_S b H2) _x y := FS y
-    }.
-Next Obligation. inversion H. Qed.
+  Equations widen_fin {n n'} (H: n<<=n') (x : Fin.t n) : Fin.t n' :=
+    widen_fin (n:=0)   (n':=0)   _H          x := x;
+    widen_fin (n:=S a) (n':=S a) (le_n a)    x := x;
+    widen_fin (n:=0)   (n':=S a) _H  x := absurd_fin x; (* absurd *)
+    widen_fin (n:=S a) (n':=0)   H           x := _; (* absurd *)
+    widen_fin (n:=S a) (n':=S b) (le_S b H1) x with widen_fin H1 x => {
+        widen_fin (n:=S a) (n':=S b) (le_S b H2) _x y := FS y
+      }.
+  Next Obligation. inversion H. Qed.
 
-(* A much nicer definition of [widen_fin] can be found here:
-   https://github.com/mattam82/Coq-Equations/blob/ded4baada11f9434333e53d9eb3c21ded33e67ad/examples/Fin.v#L58
- *)
+  (* A much nicer definition of [widen_fin] can be found here:
+     https://github.com/mattam82/Coq-Equations/blob/ded4baada11f9434333e53d9eb3c21ded33e67ad/examples/Fin.v#L58
+   *)
 
-(*
-Inductive t : nat -> Set :=  F1 : forall n : nat, t (S n) | FS : forall n : nat, t n -> t (S n).
- *)
+  (*
+    Inductive t : nat -> Set :=  F1 : forall n : nat, t (S n) | FS : forall n : nat, t n -> t (S n).
+   *)
 
-Equations le_t_inj {n m} (H : S n <<= m) : n <<= m :=
-  le_t_inj (n:=n')         le_n := le_S n' n' (le_n n');
-  le_t_inj (n:=n') (m:=?(S (S m'))) (le_S (S m') H') with le_t_inj H' => {
-      le_t_inj (n:=n') (m:=?(S (S m'))) (le_S (S m') H') H'' := le_S _ _ H''
-    }.
+  Equations le_t_inj {n m} (H : S n <<= m) : n <<= m :=
+    le_t_inj (n:=n')         le_n := le_S n' n' (le_n n');
+    le_t_inj (n:=n') (m:=?(S (S m'))) (le_S (S m') H') with le_t_inj H' => {
+        le_t_inj (n:=n') (m:=?(S (S m'))) (le_S (S m') H') H'' := le_S _ _ H''
+      }.
 
 End Helpers.
--- noc/setup.v
+++ noc/setup.v
@@ -5,11 +5,11 @@ Require Import noc.Types.
 Require Import noc.helpers.
 Require Import Coq.Vectors.Fin.
 
-Module Setup
-  (b : Typesize).
+From Equations Require Import Equations.
+Set Equations Transparent.
 
-  Module c := Types b.
-  Import c Helpers.
+
+Module Setup.
 
   (** Router state
 
@@ -58,34 +58,68 @@ Module Setup
   | rule : forall x_dim, Fin.t (S x_dim) -> rule_name_t (S x_dim).
 
 
-  Module Instances.
-
-    Definition fin_idx n (r: (reg_t (S n))) : nat :=
-      match r with
-      | router _ n state      => 2 * proj1_sig (Fin.to_nat n)
-      | router _ n downstream => 2 * proj1_sig (Fin.to_nat n) + 1
-      end.
-
-    Fixpoint fin_elems n : list (reg_t (S n)) :=
-      match n with
-      | 0 => cons (router 0 F1 state)
-             cons (router 0 F1 downstream) 
-             nil
-      | S n => cons (router n (widen_fin (@F1 n)) state)
-               cons (router n F1 downstream) 
-               fin_elems n
-      end. 
-
-
-
-Instance Fin_regt : forall n, FiniteType (reg_t (S n)) :=
-{ 
-  finite_index := fin_idx;
-  finite_elements := fin_elems;
-  finite_surjective: forall a: T, List.nth_error finite_elements (finite_index a) = Some a;
-  finite_injective: NoDup (List.map finite_index finite_elements) 
-}
-
-  End Instances.
-
 End Setup.
+
+Module Instances.
+
+  Import Setup Helpers.
+
+  Equations fin_idx {n} (r: (reg_t (S n))) : nat :=
+    fin_idx (router _ n' state)      := 2 * proj1_sig (Fin.to_nat n');
+    fin_idx (router _ n' downstream) := 2 * proj1_sig (Fin.to_nat n') + 1.
+
+  (* TODO move rename to [widen_le_t] *)
+  Equations widen_fin_left {m} (H: m <<= S (S m)) : S m <<= S (S m) :=
+    widen_fin_left (m:=m') H' := le_S _ _ (le_n (S m')).
+
+  Equations widen_fin_left0 {m} (H: 0 <<= S (S m)) : S 0 <<= S (S m) :=
+    widen_fin_left0 (m:=0)    H' := widen_fin_left H';
+    widen_fin_left0 (m:=S m') (le_S _ _ H') := le_S _ _ (widen_fin_left0 H').
+
+  Equations widen_le_t_S {n m} (H: n <<= m) : S n <<= S m :=
+    widen_le_t_S (n:=n') (m:=?(n')) (le_n ?(n')) :=  le_n (S n');
+    widen_le_t_S (n:=n') (m:=?(S m'))  (le_S n' m' H') :=
+     le_S _ _ (widen_le_t_S H').
+
+  Equations fin_elems' n m (H: n <<= m) : list (reg_t (S m)) :=
+    fin_elems' 0      0     _  :=
+      [router 0 F1 state; router 0 F1 downstream];
+
+    fin_elems' 0     (S 0) (le_S 0 ?(0) H') :=
+      let f1 := FS (@F1 0) in
+      [router (S 0) f1 state; router (S 0) f1 downstream];
+
+    fin_elems' 0     (S (S m')) H' :=
+      let f1 := FS (widen_fin (widen_fin_left0 H') (@F1 0)) in
+      [router (S (S m')) f1 state; router (S (S m')) f1 downstream];
+
+    fin_elems' (S n') (S m') H' :=
+      let f1 := widen_fin (widen_le_t_S H') (@F1 (S n')) in
+      cons (router (S m') f1 state)
+        (cons (router (S m') f1 downstream)
+          (fin_elems' n' (S m') (le_t_inj H'))).
+
+
+  Definition fin_elems {n} : list (reg_t (S n)) :=
+    fin_elems' n n (le_n n).
+
+  Lemma regt_error :
+    forall n, forall a: (reg_t (S n)),
+      List.nth_error fin_elems (fin_idx a) = Some a.
+  Proof.
+    intros n r.
+    depelim r.
+    destruct n; destruct r.
+    - destruct t.
+
+  
+
+  Instance Fin_regt : forall n, FiniteType (reg_t (S n)) :=
+  {
+    finite_index := fin_idx;
+    finite_elements := fin_elems;
+    finite_surjective: forall a: T, List.nth_error finite_elements (finite_index a) = Some a;
+    finite_injective: NoDup (List.map finite_index finite_elements)
+  }
+
+End Instances.
