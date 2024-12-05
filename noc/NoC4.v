1f5a1c9ae499bb7a70da7609ea9f4ec9a3145f9f
garvitchhabra_7 <garvit.chhabra@mailbox.tu-dresden.de>
2024-12-05 16:14:14 +0100

    Added show functions for reg_t and rule_name_t

--- noc/NoC4.v
+++ noc/NoC4.v
@@ -5,6 +5,8 @@ Require Import noc.Types.
 Require Import noc.helpers.
 Require Import noc.setup.
 Require Import Koika.Testing.
+Require Import Coq.Vectors.Fin.
+
 
 (* Require Import Coq.Strings.Byte. *)
 
@@ -37,16 +39,59 @@ Lemma P (n:nat) (H: Helpers.le_t (S n) (S nocsize)) :
     (fun ctxt =>
       let bits_r := ctxt.[router nocsize (Helpers.widen_fin H (@Fin.F1 n)) state] in
       Bits.to_nat bits_r = 0).
+    Admitted.
 
-Timeout 5 Definition package :=
+Definition show_router (r : router_reg_t) : string :=
+  match r with
+  | state => "state"
+  | downstream => "downstream"
+  end.
+
+Fixpoint fin_to_nat {n : nat} (f : Fin.t n) : nat :=
+  match f with
+  | @F1 _ => 0
+  | @FS _ f' => S (fin_to_nat f')
+  end.
+
+Definition fin_to_string {n:nat} (f: Fin.t n): string :=
+  Show_nat.string_of_nat (fin_to_nat f).
+  
+
+Definition show_reg {nocsize:nat} (reg : (reg_t (S nocsize))) : string :=
+  match reg with
+  | router n f s => ("router"  ++ (show_router s) ++ (fin_to_string f))%string
+  end.  
+
+Definition show_rule {nocsize:nat} (rule : (rule_name_t (S nocsize))) : string :=
+  match rule with
+  | rule n f => ("rule" ++ (fin_to_string f))%string
+  end.  
+
+(* Definition example_reg : reg_t 3 := router 2 (@FS 2 (@F1 1)) state.
+Definition example_rule  : rule_name_t 3 := rule 2 (@FS 2 (@F1 1)). *)
+
+Definition rule_external (rule : (rule_name_t (S nocsize))) : bool:=
+  match rule with
+  _ => false
+  end.
+
+Instance Show_reg : Show (reg_t (S nocsize)) :=
+  {| show := show_reg |}.
+
+Instance Show_rule : Show (rule_name_t (S nocsize)) :=
+  {| show := show_rule |}.
+
+Timeout 60 Definition package :=
     {|
     ip_koika :=
     {|
+    koika_reg_names := Show_reg;
     koika_reg_types := R;
     koika_reg_init := r;
     koika_ext_fn_types := Sigma;
     koika_rules := to_action;
-    koika_rule_external := false;
+    koika_rule_external := rule_external;
+    koika_rule_names := Show_rule;
     koika_scheduler := schedule;
     koika_module_name :="NoC4"
     |};
